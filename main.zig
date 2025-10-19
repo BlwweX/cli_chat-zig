adbe268aeeeb0c3171dfc5889c27e4de3f67e955
BlwweX <melvinsam003@gmail.com>
2025-10-19 16:42:05 -0600

    Function refactoring and message struct finished

--- main.zig
+++ main.zig
@@ -1,22 +1,21 @@
 const std = @import("std");
 const network = @import("network");
 const stdout = std.io.getStdOut().writer();
-const c = @cImport(@cInclude("time.h"));
-
-const timeStamp = struct {
-    hour: u64,
-    min: u64,
-    sec: u64
-};
+const clocktime = @import("clocktime.zig");
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const gpallocator = gpa.allocator();
 
 const msg = struct {
     const self = @This();
-    data: [256]u8,
-    userName: []u8,
-    sendTime: timeStamp,
+    data: []const u8,
+    userName: []const u8,
+    sendTime: clocktime.Timestamp = undefined,
     
-    pub fn send(this: self, allocator: std.mem.Allocator) ![]u8 {
-        return try std.fmt.allocPrint(allocator, "[{d}:{d}:{d}] {s}: {s}\n", .{this.sendTime.hour, this.sendTime.min, this.sendTime.sec, this.userName, this.data});
+    pub fn send(this: *self, allocator: std.mem.Allocator) ![]u8 {
+        const ourSendTime: clocktime.Timestamp = clocktime.Timestamp.localtime() catch |err| return err;
+        this.sendTime = ourSendTime;
+
+        return try std.fmt.allocPrint(allocator, "[{d}:{d}:{d}] {s}: {s}\n", .{ourSendTime.tm_hour, ourSendTime.tm_min, ourSendTime.tm_sec, this.userName, this.data});
     }
 };
 
@@ -33,7 +32,8 @@ fn readLine() ![]const u8 {
 
 
 pub fn main() !void {
-    stdout.print("{d}\n", .{std.time.timestamp()}) catch |err| {
-        return err;
-    };
+    var myMsg = msg{.userName = "exx", .data = "foobar"};
+    _ = &myMsg;
+
+    stdout.print("{s}\n", .{try myMsg.send(gpallocator)}) catch |err| return err;
 }
