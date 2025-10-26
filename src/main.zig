428de39f718fc9d35e063aaf9ae508eec10bacc8
BlwweX <melvinsam003@gmail.com>
2025-10-25 20:06:49 -0600

    Removed weird network package and started using POSIX & net structs

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,14 @@
 const std = @import("std");
-const network = @import("network");
-const stdout_writer = std.io.getStdOut().writer();
 const clocktime = @import("clocktime.zig");
+const net = std.net;
+const posix = std.posix;
+
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const gpallocator = gpa.allocator();
+
 const stdin = std.io.getStdIn();
 const reader = stdin.reader();
+const stdout_writer = std.io.getStdOut().writer();
 
 const msg = struct {
     const self = @This();
@@ -16,73 +19,79 @@ const msg = struct {
     pub fn new(this: *self, allocator: std.mem.Allocator) ![]u8 {
         const ourSendTime: clocktime.Timestamp = clocktime.Localtimestamp() catch |err| return err;
         this.sendTime = ourSendTime;
-
-        return try std.fmt.allocPrint(allocator, "[{d}:{d}:{d}] {s}: {s}\n", .{ourSendTime.tm_hour, ourSendTime.tm_min, ourSendTime.tm_sec, this.userName, this.data});
+        return try std.fmt.allocPrint(allocator, "[{d}:{d}:{d}] {s}: {s}\n",
+            .{ ourSendTime.tm_hour, ourSendTime.tm_min, ourSendTime.tm_sec, this.userName, this.data });
     }
 };
 
 fn readLine() ![]const u8 {
     const stdin_reader = std.io.getStdIn().reader();
     var result: [256]u8 = undefined;
-
     const slice = try stdin_reader.readUntilDelimiter(&result, '\n');
     const clean = std.mem.trimRight(u8, slice, "\r\n");
-
     return clean;
 }
 
-fn connectSocket(
-    writer: std.fs.File.Writer, 
-    allocator: std.mem.Allocator, 
-    port_number: u16
-) !*SocketConn {
-    const conn = try allocator.create(SocketConn);
-    errdefer allocator.destroy(conn);
+fn createListener(
+port_number: u16, 
+allocator: std.mem.Allocator, 
+writer: std.fs.File.Writer
+) !MessageSocket {
+    const address = try std.net.Address.parseIp("127.0.0.1", port_number);
 
-    conn.* = SocketConn{
-        .sock = try network.Socket.create(allocator, .udp),
+    const tpe: u32 = posix.SOCK.STREAM;
+    const protocol: comptime_int = posix.IPPROTO.TCP;
+    const listener = try posix.socket(address.any.family, tpe, protocol);
+    
+    try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
+
+    try posix.bind(listener, &address.any, address.getOsSockLen());
+
+    return MessageSocket{
+        .listener = listener,
         .writer = writer,
-        .allocator = allocator,
+        .port_num = port_number
     };
-
-    try conn.sock.bind(.{
-        .address = try network.Address.resolveIp("0.0.0.0", port_number),
-    });
-
-    try writer.print("Connected to chatroom at port: {d}\n", .{port_number});
-    return conn;
 }
 
-const SocketConn = struct {
-    sock: network.Socket,
-    writer: std.fs.File.Writer,
-    allocator: std.mem.Allocator,
+pub const MessageSocket = struct {
+    listener: std.posix.socket_t,
+    writer: std.fs.File.writer,
+    port_num: u16,
+    
+    pub fn read(self: @This()) !posix.socket_t {
+        var client_addr: net.Address = undefined;
+        var client_addr_len: posix.socklen_t = @sizeOf(net.Address);
 
-    pub fn send(this: *@This(), destination_ip: []const u8, port: u16, message: []const u8) !void {
-        const destination = try network.Address.resolveIp(destination_ip, port);
-        _ = try this.sock.sendTo(destination, message);
-    }
+        const socket = try posix.accept(self.listener,  &client_addr.any, &client_addr_len, 0);
 
-    pub fn close(this: *@This()) void {
-        this.sock.close();
-        network.deinit();
-    }
+       var buf: [128]u8 = undefined;
 
-    pub fn receive(this: *@This(), writer: std.fs.File.Writer) !void {
-        var buf: [1024]u8 = undefined;
-
-        const result = try this.sock.receiveFrom(&buf);
-        const data = buf[0..result.size];
+        const readBytes = try posix.read(socket, &buf);
         
-        try writer.print("{s}\n", .{data});
+
+        if (readBytes != 0) {
+            try self.writer.print("{s}\n", .{buf[0..readBytes]});
+        }
+
+        return socket;
     }
+
+    pub fn write(socket: posix.socket_t, writeMsg: []const u8) !void {
+        var pos: usize = 0;
+
+        while (pos < msg.len) {
+            const written = try posix.write(socket, writeMsg[pos..]);
+            if (written == 0) {
+                return error.Closed;
+            }
+            pos += written;
+        }
+    }
+       
 };
 
-
 pub fn main() !void {
-    try network.init();
-    defer network.deinit();
-
     const args = std.process.argsAlloc(gpallocator) catch |err| {
         try stdout_writer.print("Could not retrieve arguments: {s}\n", .{@errorName(err)});
         return;
@@ -95,30 +104,9 @@ pub fn main() !void {
     }
 
     const port = try std.fmt.parseInt(u16, args[1], 10);
-
     try stdout_writer.print("Enter username: \n", .{});
     var buf: [100]u8 = undefined;
 
     const line = try reader.readUntilDelimiterOrEof(&buf, '\n');
-    const username = if (line) |usrnme| usrnme else []const u8{"Anonymous"};
-
-
-    const SocketConnection = try connectSocket(stdout_writer, gpallocator, port);
-    defer SocketConnection.close();
-
-    
-    while (true) {
-        try SocketConnection.receive(stdout_writer);
-
-        try stdout_writer.print("[{s}]: ", .{username});
-        const msgLine = try reader.readUntilDelimiterOrEof(&buf, '\n');
-
-        if (msgLine) |writtenMsg| {
-            const ourMsg = msg{ .data = writtenMsg, .userName = username };
-            const text = try ourMsg.new(gpallocator);
-            try SocketConnection.send("127.0.0.1", port, text);
-            gpallocator.free(text);
-        }
-    }
+    const username = if (line) |usrnme| usrnme else "Anonymous"; 
 }
-
